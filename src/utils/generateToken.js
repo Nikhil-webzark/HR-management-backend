55abfd2f7e574758c44632f42b1dc5ca4a56c7b6
Nikhil kumar <kumar.nikhil@webzark.tech>
2026-07-01 13:20:11 +0530

    cookie issue fixed

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, user) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
