d8f953129855cd51c819bbb68e49eb6a6bc3b1a4
Nikhil kumar <kumar.nikhil@webzark.tech>
2026-07-01 11:09:53 +0530

    added frontend url

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 
 // CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: "https://hr-management-frontend-iota.vercel.app/",
   credentials: true,
 }));
 
